9c47d0c0ee534b2a8abf744c5a4624e6993d0af9
yuxitong <yuxitong_al@163.com>
2016-08-05 15:58:58 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   /*global mocha,chai,EventEmitter*/
   'use strict';
 
@@ -8,67 +8,47 @@
 
   function flattenCheck(check) {
     var sorted = check.slice(0);
-    sorted.sort(function (a, b) {
+    sorted.sort(function(a, b) {
       return a < b ? -1 : 1;
     });
     return sorted.join();
   }
 
   // Configure the tests
-  suite('getListeners', function () {
+  suite('addListeners', function() {
     var ee;
 
-    setup(function () {
-      ee = new EventEmitter();
+    setup(function() {
+      ee = new Event();
     });
 
-    test('initialises the event object and a listener array', function () {
-      ee.getListeners('foo');
+    test('initialises the event object and a listener array', function() {
+      ee.on('foo');
       assert.deepEqual(ee._events, {
         foo: []
       });
     });
-
-    test('does not overwrite listener arrays', function () {
-      var listeners = ee.getListeners('foo');
-      listeners.push('bar');
-
-      assert.deepEqual(ee._events, {
-        foo: ['bar']
-      });
-
-      ee.getListeners('foo');
-
-      assert.deepEqual(ee._events, {
-        foo: ['bar']
-      });
-    });
-
-    test('allows you to fetch listeners by regex', function () {
-      var check = [];
-
-      ee.addListener('foo', function () { check.push(1); });
-      ee.addListener('bar', function () { check.push(2); return 'bar'; });
-      ee.addListener('baz', function () { check.push(3); return 'baz'; });
-
-      var listeners = ee.getListeners(/ba[rz]/);
-
-      assert.strictEqual(listeners.bar.length + listeners.baz.length, 2);
-      assert.strictEqual(listeners.bar[0].listener(), 'bar');
-      assert.strictEqual(listeners.baz[0].listener(), 'baz');
-    });
-
-    test('does not return matched sub-strings', function () {
-      var check = function () { };
-
-      ee.addListener('foo', function () { });
-      ee.addListener('fooBar', check);
-
-      var listeners = ee.getListeners('fooBar');
-      assert.strictEqual(listeners.length, 1);
-      assert.strictEqual(listeners[0].listener, check);
-    });
   });
+
+  suite('trigger', function() {
+    var ee;
+
+    setup(function() {
+      ee = new Event();
+    })
+
+    test('trigger the event', function() {
+      var run = false;
+
+      ee.on('foo', function() {
+        run = true;
+      })
+
+      ee.trigger('foo')
+
+      assert.isTrue(run);
+    })
+  })
 
   // Execute the tests.
   mocha.run();
